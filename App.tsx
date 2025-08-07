d60ef7bfb0455f1856665f3e8fa4a0973220ea62
SUBACITO <subacitodev@gmail.com>
2025-08-07 19:03:12 +0700

    'init'

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+export default function App() {
+  
+
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to suck world!</Text>
+      <StatusBar style="auto" />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
